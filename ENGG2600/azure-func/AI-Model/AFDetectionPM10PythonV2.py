0139420c77d9b4222536325843cfc18e887b773f
DuncanBrady <robert.duncan.brady@gmail.com>
2021-07-22 22:17:31 +1000

    completed functionality for handling dat files and json arrays, refactored load model to remove need for preProcess.py, integrated rezas code to loadModel.py but did not transfer the new model to the code

---
+++ ENGG2600/azure-func/AI-Model/AFDetectionPM10PythonV2.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  5 19:56:29 2021
+
+@author: Reza
+"""
+
+import numpy as np
+from tensorflow.keras.models import load_model
+from scipy.signal import butter, lfilter, resample
+from scipy import signal
+from scipy.signal import convolve as sig_convolve
+## loading the model
+# Set this to the model's path
+Model = load_model('C:\\Users\\Reza\\anaconda3\\ModelTrainedOnCambellSamples.h5')
+
+## Reading dat file
+# Set this path
+path_dat = 'D:\\Nigel Projects\AF Detection\ecgSamplesNew\\NewSamplesCambell\\Patient 15F (Lead III) - AF\\'
+
+fin = open(path_dat+'ecg.dat', 'rb')
+a = np.fromfile(fin, dtype=np.dtype('<u2'))
+X=[]
+for x in a:
+  #  print(x)
+    X.append(x)
+new_sample = np.asarray(X)
+
+
+## Filtering
+
+from scipy import signal
+from scipy.signal import convolve as sig_convolve
+temp1 = signal.medfilt(new_sample, kernel_size=np.int64(250/5+1))
+temp2 = signal.medfilt(temp1, kernel_size=np.int64(2*250/3+1))
+new_sample_subt = new_sample-temp2
+b = signal.firwin(21,  20/250 , window='hamming')
+new_sample_flt = sig_convolve(np.expand_dims(new_sample_subt,axis=0), b[np.newaxis, :], mode='valid')
+
+
+# Normalisation
+def Normalize(data):
+    maxValue = np.max(data)
+    minValue = np.min(data)
+    temp = (data-minValue)/(maxValue-minValue)
+    return temp
+new_sample_flt_norm = np.squeeze(np.transpose(Normalize(new_sample_flt)))
+
+
+# Cutting and Padding
+def embeddingTupel(data,maxLength):
+    maxLength = np.int(maxLength)
+    temp = np.full((maxLength-data.shape[0],),0)
+    dataEmb = np.concatenate((data,temp.reshape(temp.shape[0],)),axis=0)
+    return dataEmb
+
+if new_sample_flt_norm.shape[0]>7500:
+    new_sample_flt_norm_resize = new_sample_flt_norm[0:7500,:]
+else: 
+    new_sample_flt_norm_resize = embeddingTupel((new_sample_flt_norm),7500)
+    
+    
+# Upsampling
+new_sample_flt_norm_resize_upsamp = resample(new_sample_flt_norm_resize, 9000)
+
+## This is the new line to pad the samples to 60 sec
+new_sample_flt_norm_resize_upsamp_padd = embeddingTupel(new_sample_flt_norm_resize_upsamp,18000)
+
+predProb = Model.predict(np.expand_dims(np.expand_dims(new_sample_flt_norm_resize_upsamp_padd,axis=1),axis=0))
+Prediction = np.argmax(predProb)+1
+def second_largest(array):
+    sortedgivenArray = sorted(array, reverse = True)
+    secondLargestNumber = sortedgivenArray[1]
+    return secondLargestNumber
+Reject = 0
+if np.max(predProb)<0.97 and (np.max(predProb)-second_largest(predProb))<0.95:
+    Reject = 1
+    
+print(Prediction)
+print(Reject)
--- ENGG2600/azure-func/AI-Model/__init__.py
+++ ENGG2600/azure-func/AI-Model/__init__.py
@@ -1,47 +1,38 @@
+from io import open_code
 from json.decoder import JSONDecodeError
 import logging
 import sys
+
+from tensorflow.python.platform.tf_logging import log_first_n
 import azure.functions as func
 import numpy as np
-from json import loads, dumps
 from .loadModel import getPrediction
 from tensorflow.keras.models import load_model
 import os
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    '''
-    if jsonData:
-        logging.info("Sample data has been found")
-        string1 = str(type(jsonData))
-        string2 = str(len(jsonData))
-        array = np.array(jsonData)
-        string3 = str(type(array))
-        logging.info(string1 + string2 + string3)
-        return func.HttpResponse("data has been found and return the otucome")
-    sampleNum = req.params.get('sample')
-    # logging.info(sampleNum)
-    if not sampleNum:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            sampleNum = req_body.get('sample')
-    '''
+    if req.files.values() is not None:
+        for input_file in req.files.values():
+            logging.info(input_file.name)
+            file = input_file.stream
+            sampleData = np.fromfile(file, dtype=np.dtype('<u2'))
     try:
-        req_body = req.get_json()
+        rawData = req.get_json()
+        sampleData = np.asarray(rawData)
     except ValueError:
+        logging.info("value error for Raw Data")
         pass
-    else: 
-        #this is not complete the function needs to check for the sample file or list
-        sampleData = req_body
-    if sampleData:
+
+    if sampleData is not None:
         # Load model
         model = load_model('./AI-Model/ECGClassificationModel.h5')
+        dataset = []
+        for x in sampleData:
+            dataset.append(x)
         #load the sample as a numpy array and pass to model
-        dataset = np.asarray(sampleData)
-        prediction = getPrediction(model, dataset)
+        sampData = np.asarray(dataset)
+        prediction = getPrediction(model, sampData)
         logging.info(prediction)
 
         return func.HttpResponse(f"The prediction for Sample is '{prediction}'.")
